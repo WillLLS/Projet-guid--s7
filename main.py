c8e5e0eef607fafb748a5e30f53b6c01ff71c0bc
William <lalis.william@gmail.com>
2021-12-08 16:39:02 +0100

    blink  led

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 """
     Installation plug in pymakr pour téléversement vers pycom lopy4
+    blink led code :
 """
+
+import pycom
+import time
+
+pycom.heartbeat(False)
+for cycles in range(10): # stop after 10 cycles
+    pycom.rgbled(0x007f00) # green
+    time.sleep(1)
+    pycom.rgbled(0x7f7f00) # yellow
+    time.sleep(1)
+    pycom.rgbled(0x7f0000) # red
+    time.sleep(1)
